24b6f5ccf77ec45a871c05a95b070ba8e0fa52c4
Archit <jainarchit1604@gmail.com>
2024-02-18 12:56:15 +0530

    Fixed

--- src/components/Scan.jsx
+++ src/components/Scan.jsx
@@ -123,7 +123,7 @@ const Scan = () => {
             .catch((error) => {
               console.error(error.text);
             });
-          navigate("/details" + emaily);
+          navigate("/details/" + emaily);
         } else {
           console.log("No file selected");
         }
